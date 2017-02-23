86f5244f3b51751e32d4338f46a3c66fca1497d2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-23 12:16:30 -0500

    use snap-shot to test actions

--- todos/src/actions/index.spec.js
+++ todos/src/actions/index.spec.js
@@ -1,25 +1,16 @@
 import * as actions from './index'
+import snapshot from 'snap-shot'
 
 describe('todo actions', () => {
   it('addTodo should create ADD_TODO action', () => {
-    expect(actions.addTodo('Use Redux')).toEqual({
-      type: 'ADD_TODO',
-      id: 0,
-      text: 'Use Redux'
-    })
+    snapshot(actions.addTodo('Use Redux'))
   })
 
   it('setVisibilityFilter should create SET_VISIBILITY_FILTER action', () => {
-    expect(actions.setVisibilityFilter('active')).toEqual({
-      type: 'SET_VISIBILITY_FILTER',
-      filter: 'active'
-    })
+    snapshot(actions.setVisibilityFilter('active'))
   })
 
   it('toggleTodo should create TOGGLE_TODO action', () => {
-    expect(actions.toggleTodo(1)).toEqual({
-      type: 'TOGGLE_TODO',
-      id: 1
-    })
+    snapshot(actions.toggleTodo(1))
   })
 })
