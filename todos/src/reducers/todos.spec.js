2703acfc7baf22a7d6c38056c21ade96509ead3c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-23 12:41:21 -0500

    snap-shot data-driven testing

--- todos/src/reducers/todos.spec.js
+++ todos/src/reducers/todos.spec.js
@@ -1,96 +1,55 @@
-import { test, given } from 'sazerac'
 import todos from './todos'
+import snapshot from 'snap-shot'
 
-test(todos, () => {
+// a helper function for clarity to avoid all the nested arrays
+const given = (state, action) => [state, action]
+// we could even go further if needed
+// given(state(...items), action) => [items, action] or something
 
-  given(undefined, {}).expect([])
+// we can use in a couple of tests
+const addAction = {
+  type: 'ADD_TODO',
+  text: 'Run the tests',
+  id: 0
+}
 
-  given([], {
-    type: 'ADD_TODO',
+it('updates todos based on actions', () => {
+  const runTodo = {
     text: 'Run the tests',
+    completed: false,
     id: 0
-  })
-  .expect([
-    {
-      text: 'Run the tests',
-      id: 0,
-      completed: false
-    }
-  ])
+  }
 
-  given([], {
-    type: 'ADD_TODO',
-    text: 'Run the tests',
-    id: 0
-  })
-  .describe('when state is empty and a todo is added')
-  .assert('should add the todo and set `id` property', (state) => {
-    expect(state[0].id).toEqual(0)
-  })
-  .assert('should add the todo and set `text` property', (state) => {
-    expect(state[0].text).toEqual('Run the tests')
-  })
-  .assert('should set completed:false on the added todo', (state) => {
-    expect(state[0].completed).toEqual(false)
-  })
-  .assert('should add only one todo', (state) => {
-    expect(state.length).toEqual(1)
-  })
-
-  given([
-    {
-      text: 'Run the tests',
-      completed: false,
-      id: 0
-    }
-  ], {
+  const useAction = {
     type: 'ADD_TODO',
     text: 'Use Redux',
     id: 1
-  })
-  .describe('when state has an existing todo and another todo is added')
-  .assert('should add the todo after the existing todo', (state) => {
-    expect(state[1].id).toEqual(1)
-  })
-  .assert('should add only one todo', (state) => {
-    expect(state.length).toEqual(2)
-  })
+  }
 
-  given([
-    {
-      text: 'Run the tests',
-      completed: false,
-      id: 1
-    }, {
-      text: 'Use Redux',
-      completed: false,
-      id: 0
-    }
-  ], {
+  const study = {
+    text: 'Study hard',
+    completed: false,
+    id: 1
+  }
+
+  const toggleAction = {
     type: 'TOGGLE_TODO',
     id: 1
-  })
-  .describe('when a todo with completed:false is toggled')
-  .assert('should set complete:true on the toggled todo', (state) => {
-    expect(state[0].completed).toEqual(true)
-  })
-  .assert('should not toggle any other todos', (state) => {
-    expect(state[1].completed).toEqual(false)
-  })
+  }
 
-  given([
-    {
-      text: 'Run the tests',
-      completed: true,
-      id: 1
-    }
-  ], {
-    type: 'TOGGLE_TODO',
-    id: 1
-  })
-  .describe('when a todo with completed:true is toggled')
-  .assert('should set complete:false on the toggled todo', (state) => {
-    expect(state[0].completed).toEqual(false)
-  })
+  snapshot(todos,
+    given(undefined, {}),
+    given([], addAction),
+    given([runTodo], useAction),
+    given([runTodo, useAction], toggleAction),
+    // toggles same item twice
+    // I would use a "normal" separate test here
+    given(todos([study], toggleAction), toggleAction)
+  )
+})
 
+// detailed error messages are probably better as test names
+it('sets "id", "test" and "completed" properties', () => {
+  snapshot(todos([], addAction))
+  // also implicitly confirms that only a single item in the output list
 })
